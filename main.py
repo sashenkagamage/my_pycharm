3746c66c23a9014a09d9c63f1f035680c9b5f2b0
sashenkagamage <sashenka.gamage@outlook.com>
2021-06-19 11:58:10 +0530

    new function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-.idea
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -8,11 +8,14 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def my_function(a,b):
+    return a+b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('how are you ?')
+    print(my_function(2,4))
 
+    x=30
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
